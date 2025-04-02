7bd543e1cb3005e3f238d1890c7dae85557d8661
victoriatobio <vtobio@udesa.edu.ar>
2025-04-02 17:46:48 -0300

    rutas

--- proyecto/routes/login.js
+++ proyecto/routes/login.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('login',);
 });
 
-
+module.exports = router;
