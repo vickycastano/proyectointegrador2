a7c8574c33065f17cf15ef9b74bd52224c1c6cbd
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-05-19 12:16:16 -0300

    estructura basica de el punto 1

---
+++ proyecto/database/models/Comentario.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes){
+
+    let alias = ""
+    let cols={
+
+
+
+     let config = {
+
+    }   
+    }
+
+
+
+}
+
+let Producto = sequelize.define(alias,col,config);
+return Producto 
