12762c65d23544e1b2714320d5f24a859451a663
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-05-26 19:20:15 -0300

    correcion de ruta de productadd

--- proyecto/routes/producto.js
+++ proyecto/routes/producto.js
@@ -5,4 +5,6 @@ const productoController = require('../controllers/productoController')
 //detalle de producto 
 router.get('/product/:id', productoController.detail);
 
+router.get('/productadd',productoController.productadd)
+
 module.exports = router;
