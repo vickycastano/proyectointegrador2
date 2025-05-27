788fd26ea1497cc63ccf8f0e218ccd8f7886c2a2
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-05-27 17:00:51 -0300

    cambio en app.js

--- proyecto/app.js
+++ proyecto/app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
   if (req.cookies.usuarioEmail != undefined && req.session.user == undefined) {
     res.locals.user = req.cookies.usuarioEmail
-    req.session.usuarioLogueado = req.cookies.usuarioEmail
+    req.session.user = req.cookies.usuarioEmail
   }
   return next();
 });
